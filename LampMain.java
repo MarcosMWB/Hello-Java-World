835bb5c2b12271290c6a2c805568feb813fee20d
Marcos de Mello Wermelinger Bittencourt <marcosmwb1919@gmail.com>
2018-03-08 19:28:54 -0300

    Create LampMain.java

---
+++ LampMain.java
@@ -0,0 +1,16 @@
+package projLamp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Lamp lamp = new Lamp();
+		
+		lamp.turn_on();
+		
+		lamp.showStatus();
+
+	}
+
+}
